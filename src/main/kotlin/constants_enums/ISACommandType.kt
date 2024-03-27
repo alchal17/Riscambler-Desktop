a53e161b1ec82e9a63f264e46873e22a39b4d57c
bevz-pn <bevz.pn@ucu.edu.ua>
2024-03-27 23:33:03 +0200

    Command encoder ("compiler") almost finished, constants updated

--- src/main/kotlin/constants_enums/ISACommandType.kt
+++ src/main/kotlin/constants_enums/ISACommandType.kt
@@ -1,10 +1,10 @@
 package constants_enums
 
-enum class ISACommandType(val opcode: UInt) {
-    R(0b0110011u),
-    I(0b0010011u),
-    S(0b0100011u),
-    SB(0b1100011u),
-    U(0b0110111u),
-    UJ(0b1101111u)
+enum class ISACommandType(val opcode: String) {
+    R("0110011"),
+    I("0010011"),
+    S("0100011"),
+    SB("1100011"),
+    U("0110111"),
+    UJ("1101111")
 }
